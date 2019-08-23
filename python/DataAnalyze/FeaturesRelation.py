b2a705f1475bf612a77f1dad08894372cd5d3485
Tomas Kello <kello.tomas@gmail.com>
2019-08-23 16:24:10 +0300

    Python script generated all graphs

--- python/DataAnalyze/FeaturesRelation.py
+++ python/DataAnalyze/FeaturesRelation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pyodbc
 
 # Create plot and data
 
@@ -8,22 +9,42 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
 def addPoint(vagony, hmot, dlzka, napravy):
-    datanew[0][0] = np.append(datanew[0][0], vagony)
-    datanew[0][1] = np.append(datanew[0][1], hmot)
-    datanew[1][0] = np.append(datanew[1][0], vagony)
-    datanew[1][1] = np.append(datanew[1][1], dlzka)
-    datanew[2][0] = np.append(datanew[2][0], vagony)
-    datanew[2][1] = np.append(datanew[2][1], napravy)
+    if (vagony != 0):
+        if (hmot != 0):
+            datanew[0][0] = np.append(datanew[0][0], vagony)
+            datanew[0][1] = np.append(datanew[0][1], hmot)
+        if (dlzka != 0):
+            datanew[1][0] = np.append(datanew[1][0], vagony)
+            datanew[1][1] = np.append(datanew[1][1], dlzka)
+        if (napravy != 0):
+            datanew[2][0] = np.append(datanew[2][0], vagony)
+            datanew[2][1] = np.append(datanew[2][1], napravy)
 
 def addTrend(x, y, color):
     p = np.poly1d(np.polyfit(x, y, 1))
     ax.plot(x, p(x), color)
-    
-for i in range(0, 20):
-    addPoint(i, i+np.random.rand(1)*3, i*2+np.random.rand(1)*3, i*0.5+np.random.rand(1)*3)
 
+def dbExecute(query):
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=dokelu.kst.fri.uniza.sk;DATABASE=TrainsDb19-04-02;UID=kello4;PWD=kello4')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor
 
-for data, color, group in zip(datanew, ("red", "green", "blue"), ("hmotnost", "dlzka", "pocet naprav")):
+trainType = 'Nex'
+#cursor = dbExecute('SELECT MAX(PocVoznov), MAX(Hmot), MAX(Dlzka), MAX(PocNaprav) FROM [TrainsDb19-04-02].[dbo].[Trains] WHERE druh = \'' + trainType + '\' group by vlakid')
+cursor = dbExecute('SELECT MAX(PocVoznov), MAX(Hmot), MAX(Dlzka), MAX(PocNaprav) FROM [TrainsDb19-04-02].[dbo].[Trains] WHERE druh = \'Lv\' OR druh = \'Sv\' OR druh = \'Sluz\' OR druh = \'Mn\' OR druh = \'Sp\' OR druh = \'Vlec\' OR druh = \'PMD\' group by vlakid')
+
+step = 1000;
+count = 0;
+limit = step;
+for row in cursor:
+    count = count + 1
+    addPoint(row[0], row[1], row[2], row[3]);
+    if count>=limit:
+        limit = limit + step
+        print(count)
+
+for data, color, group in zip(datanew, ("red", "green", "blue"), ("weight ("+str(len(datanew[0][0]))+")", "lenght ("+str(len(datanew[1][0]))+")", "number of axles ("+str(len(datanew[2][0]))+")")):
     x, y = data
     ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
 
@@ -33,7 +54,9 @@ addTrend(datanew[0][0], datanew[0][1], "r--")
 addTrend(datanew[1][0], datanew[1][1], "g--")
 addTrend(datanew[2][0], datanew[2][1], "b--")
 
-plt.title('Matplot scatter plot')
+#plt.title('Relations in selected numeric features \n' + trainType + ' trains')
+plt.title('Relations in selected numeric features \nother trains')
 plt.legend(loc=2)
+plt.xlabel("Number of wagons")
 plt.show()
 
