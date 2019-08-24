90eb421e467eec1c39ed8308eb457dbfef517eef
Tomas Kello <kello.tomas@gmail.com>
2019-08-24 13:41:58 +0300

    Update and rename FeaturesRelation.py to FeaturesRelationXY.py

--- python/DataAnalyze/FeaturesRelationXY.py
+++ python/DataAnalyze/FeaturesRelationXY.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyodbc
 
-# Create plot and data
+trainTypes = ['Pn']
 
 datanew = ([list(),list()], [list(),list()], [list(),list()])
 fig = plt.figure()
@@ -22,41 +22,41 @@ def addPoint(vagony, hmot, dlzka, napravy):
 
 def addTrend(x, y, color):
     p = np.poly1d(np.polyfit(x, y, 1))
+    yCentered = [i-p[0] for i in y]
+    p = np.poly1d(np.polyfit(x, yCentered, 1))
     ax.plot(x, p(x), color)
+    return 'y = {0:.2f}x'.format(p[1])
 
 def dbExecute(query):
+    print(query)
     conn = pyodbc.connect('DRIVER={SQL Server};SERVER=dokelu.kst.fri.uniza.sk;DATABASE=TrainsDb19-04-02;UID=kello4;PWD=kello4')
     cursor = conn.cursor()
     cursor.execute(query)
     return cursor
 
-trainType = 'Nex'
-#cursor = dbExecute('SELECT MAX(PocVoznov), MAX(Hmot), MAX(Dlzka), MAX(PocNaprav) FROM [TrainsDb19-04-02].[dbo].[Trains] WHERE druh = \'' + trainType + '\' group by vlakid')
-cursor = dbExecute('SELECT MAX(PocVoznov), MAX(Hmot), MAX(Dlzka), MAX(PocNaprav) FROM [TrainsDb19-04-02].[dbo].[Trains] WHERE druh = \'Lv\' OR druh = \'Sv\' OR druh = \'Sluz\' OR druh = \'Mn\' OR druh = \'Sp\' OR druh = \'Vlec\' OR druh = \'PMD\' group by vlakid')
+if len(trainTypes) == 0:
+	cursor = dbExecute('SELECT MAX(PocVoznov), MAX(Hmot), MAX(Dlzka), MAX(PocNaprav) FROM [TrainsDb19-04-02].[dbo].[Trains] group by vlakid')
+else:
+	cursor = dbExecute('SELECT MAX(PocVoznov), MAX(Hmot), MAX(Dlzka), MAX(PocNaprav) FROM [TrainsDb19-04-02].[dbo].[Trains] WHERE druh = \'' + '\' OR druh = \''.join(trainTypes) + '\' group by vlakid')
 
 step = 1000;
 count = 0;
 limit = step;
 for row in cursor:
-    count = count + 1
     addPoint(row[0], row[1], row[2], row[3]);
+    count = count + 1
     if count>=limit:
         limit = limit + step
         print(count)
 
-for data, color, group in zip(datanew, ("red", "green", "blue"), ("weight ("+str(len(datanew[0][0]))+")", "lenght ("+str(len(datanew[1][0]))+")", "number of axles ("+str(len(datanew[2][0]))+")")):
+for data, color, group in zip(datanew, ("red", "green", "blue"),("weight (" + addTrend(datanew[0][0], datanew[0][1], "r--") + ")", "lenght (" + addTrend(datanew[1][0], datanew[1][1], "g--") + ")", "number of axles (" + addTrend(datanew[2][0], datanew[2][1], "b--") + ")")):
     x, y = data
     ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
 
-# Create and plot Trend
-
-addTrend(datanew[0][0], datanew[0][1], "r--")
-addTrend(datanew[1][0], datanew[1][1], "g--")
-addTrend(datanew[2][0], datanew[2][1], "b--")
-
-#plt.title('Relations in selected numeric features \n' + trainType + ' trains')
-plt.title('Relations in selected numeric features \nother trains')
+if len(trainTypes) == 0:
+	plt.title('Relations in selected numeric features\n' + str(len(datanew[0][0])) + ' records')
+else:
+	plt.title('Relations in selected numeric features \n' + ', '.join(trainTypes) + ' trains\n' + str(len(datanew[0][0])) + ' records')
 plt.legend(loc=2)
 plt.xlabel("Number of wagons")
 plt.show()
-
