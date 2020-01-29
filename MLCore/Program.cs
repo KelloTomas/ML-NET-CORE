e9b6e37f56bd4f5619991e4e9be5e48ff095bd79
KelloTomas <kello.tomas@gmail.com>
2020-01-29 11:17:18 +0100

    refactor program.cs

--- MLCore/Program.cs
+++ MLCore/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MLCore
 {
@@ -12,42 +14,13 @@ namespace MLCore
 	{
 		private static void Main(string[] args)
 		{
-			string myDirPath = @"C:\vlaky\archiv_treko2017_2";
-			using (var db = new TrainsDb_200123Context())
-			{
-				DirectoryInfo d = new DirectoryInfo(myDirPath);//Assuming Test is your Folder
-				FileInfo[] Files = d.GetFiles("*.csv");
-				foreach (FileInfo file in Files)
-				{
-					FileHelpers.FileHelperEngine<CzVelib> engine = new FileHelpers.FileHelperEngine<CzVelib>(Encoding.GetEncoding("ISO-8859-1"));
-					engine.HeaderText = engine.GetFileHeader().Remove(0, 3);
-					engine.ErrorManager.ErrorMode = FileHelpers.ErrorMode.SaveAndContinue;
-					CzVelib[] trains = engine.ReadFile(file.FullName);
-					Array.ForEach(trains, e => e.Id = 0);
-					db.AddRange(trains);
-					db.SaveChanges();
-					/*
-					int count = 0;
-					foreach (var train in trains)
-					{
-						count++;
-						train.Id = 0;
-						db.CzPreosGtn.Add(train);
-						db.SaveChanges();
-					}
-					*/
-					if (engine.ErrorManager.HasErrors)
-						engine.ErrorManager.SaveErrors($"{myDirPath}\\err-{file.Name}.out");
-					Console.WriteLine($"Importet file: {file.Name}");
-					//Console.WriteLine($"Records: {count}");
-					//ReadFileLineByLine(db, file);
-				}
-
-				Console.WriteLine("finished");
-				Console.ReadLine();
-				return;
-			}
-
+			var f = new Functions();
+			Thread t1=new Thread(f.ExportCsvToMySQLAll);
+    		t1.Start();
+			t1.Join();
+			Console.WriteLine("Program finished");
+			Console.ReadLine();
+			return;
 
 
 			DateTime dt = DateTime.Now;
@@ -67,8 +40,75 @@ namespace MLCore
 			Console.WriteLine($"\n\n Time to calculate: {ts.ToString(@"hh\:mm\:ss\.ffff")}");
 			Console.ReadLine();
 		}
+		public static void WriteCSV<T>(IEnumerable<T> items, string path)
+		{
+			Type itemType = typeof(T);
+			var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+								.OrderBy(p => p.Name);
 
-		private static void ReadFileLineByLine(TrainsDb_200123Context db, FileInfo file)
+			using (var writer = new StreamWriter(path))
+			{
+				writer.WriteLine(string.Join(", ", props.Select(p => p.Name)));
+
+				foreach (var item in items)
+				{
+					writer.WriteLine(string.Join(", ", props.Select(p => p.GetValue(item))));
+				}
+			}
+		}
+	}
+	public class Functions
+	{
+		public async void ExportCsvToMySQLAll()
+		{
+			await ExportCsvToMySQL<SkBb>(@"C:\vlaky\archiv_BanskaBystrica");
+			await ExportCsvToMySQL<SkCaMySql>(@"C:\vlaky\archiv_cadca_mysql");
+			await ExportCsvToMySQL<SkCaOracle>(@"C:\vlaky\archiv_cadca_oracle");
+			await ExportCsvToMySQL<SkKrasnoNkysMySql>(@"C:\vlaky\archiv_krasno_mysql");
+			await ExportCsvToMySQL<SkKrasnoNkysOracle>(@"C:\vlaky\archiv_krasno_oracle");
+			await ExportCsvToMySQL<SkKutyMySql>(@"C:\vlaky\archiv_kuty_mysql");
+			await ExportCsvToMySQL<SkKutyOracle>(@"C:\vlaky\archiv_kuty_oracle");
+			await ExportCsvToMySQL<CzVelib>(@"C:\vlaky\archiv_velib1");
+			await ExportCsvToMySQL<CzTreko>(@"C:\vlaky\archiv_treko2017_2");
+		}
+		public async Task ExportCsvToMySQL<T>(string myDirPath) where T: class
+		{
+			using (var db = new TrainsDb_200123Context())
+			{
+				DirectoryInfo d = new DirectoryInfo(myDirPath);//Assuming Test is your Folder
+				FileInfo[] Files = d.GetFiles("*.csv");
+				foreach (FileInfo file in Files)
+				{
+					FileHelpers.FileHelperEngine<T> engine = new FileHelpers.FileHelperEngine<T>(Encoding.GetEncoding("ISO-8859-1"));
+					engine.HeaderText = engine.GetFileHeader().Remove(0, 3);
+					engine.ErrorManager.ErrorMode = FileHelpers.ErrorMode.SaveAndContinue;
+					T[] trains = engine.ReadFile(file.FullName);
+					//Array.ForEach(trains, e => e.Id = 0);
+					await db.AddRangeAsync(trains);
+					await db.SaveChangesAsync();
+					/*
+					int count = 0;
+					foreach (var train in trains)
+					{
+						count++;
+						train.Id = 0;
+						db.CzPreosGtn.Add(train);
+						db.SaveChanges();
+					}
+					*/
+					if (engine.ErrorManager.HasErrors)
+						engine.ErrorManager.SaveErrors($"{myDirPath}\\err-{file.Name}.out");
+					Console.WriteLine($"Importet file: {file.Name}");
+					//Console.WriteLine($"Records: {count}");
+					//ReadFileLineByLine(db, file);
+				}
+
+				Console.WriteLine($"finished: {myDirPath}");
+				return;
+			}
+		}
+
+		public void ReadFileLineByLine(TrainsDb_200123Context db, FileInfo file)
 		{
 			using (var sr = new StreamReader(file.FullName, Encoding.GetEncoding("ISO-8859-1")))
 			{
@@ -96,22 +136,6 @@ namespace MLCore
 			}
 		}
 
-		public static void WriteCSV<T>(IEnumerable<T> items, string path)
-		{
-			Type itemType = typeof(T);
-			var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-								.OrderBy(p => p.Name);
-
-			using (var writer = new StreamWriter(path))
-			{
-				writer.WriteLine(string.Join(", ", props.Select(p => p.Name)));
-
-				foreach (var item in items)
-				{
-					writer.WriteLine(string.Join(", ", props.Select(p => p.GetValue(item))));
-				}
-			}
-		}
 	}
 }
 
