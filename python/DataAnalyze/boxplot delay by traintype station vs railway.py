a457dcc1beb6148b353d757aace0e8e9e99b125e
Tomas Kello <kello.tomas@gmail.com>
2021-01-26 20:23:42 +0100

    Add files via upload

---
+++ python/DataAnalyze/boxplot delay by traintype station vs railway.py
@@ -0,0 +1,29 @@
+#from string import ascii_letters
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pyodbc
+sns.set(style="white")
+dbName = 'Kello'
+
+dbTableName = 'CZPREOS'
+dbTrainType = ['Ex', 'Lv', 'Mn', 'Nex', 'Os', 'Pn', 'R', 'Sv']
+
+conn = pyodbc.connect('DRIVER={SQL Server};SERVER=dokelu.kst.fri.uniza.sk;DATABASE=' + dbName + ';UID=read;PWD=read')
+
+df = pd.DataFrame(columns = ['TrainType', 'Place', 'Delay'])
+
+for trainType in dbTrainType:
+	q = '''select TrainType, 'Station' as Place, Delay from ''' + dbTableName + '''stanica where Delay < 2000 and Delay > -2000 and TrainType = \'''' + trainType + '\''
+	x = pd.read_sql_query(q, conn)
+	df = df.append(x)
+
+	q = '''select TrainType, 'Railway' as Place, Delay from ''' + dbTableName + ''' where Delay < 2000 and Delay > -2000 and TrainType = \'''' + trainType + '\''
+	x = pd.read_sql_query(q, conn)
+	df = df.append(x)
+
+	print(trainType + " finished")
+
+sns.boxplot(x='TrainType', y='Delay', hue='Place', data=df, showfliers=False)
+plt.ylabel('Delay [s]')
+plt.show()
--- python/DataAnalyze/boxplot delay in station by station.py
+++ python/DataAnalyze/boxplot delay in station by station.py
@@ -4,30 +4,31 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pyodbc
+import itertools
+
 sns.set(style="white")
 dbName = 'Kello'
 
-dbTableNames = ['[CZPREOSstanica]']
+dbTableName = '[CZPREOSstanica]'
 dbStationsId = ['34164400', '34662700', '38062600', '38274700', '34804500', '34544700', '34694000', '38314100', '33654500', '33722000', '34042200', '33472200', '34652800', ]
 
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=dokelu.kst.fri.uniza.sk;DATABASE=' + dbName + ';UID=read;PWD=read')
 
-for dbTableName in dbTableNames:
-	a = []
-	dbStationsWithCount = []
-	for stationId in dbStationsId:
-		q = '''select [Delay] from ''' + dbTableName + ''' where Delay < 2000 and Delay > -2000 and InSR70 = \'''' + stationId + '\''
-		print(q)
-		x = pd.read_sql_query(q, conn)['Delay'].values.tolist()
-		a.append(x)
+a = []
+dbStationsWithCount = []
+for stationId in dbStationsId:
+	q = '''select [Delay] from ''' + dbTableName + ''' where Delay < 2000 and Delay > -2000 and InSR70 = \'''' + stationId + '\''
+	print(q)
+	x = pd.read_sql_query(q, conn)['Delay'].values.tolist()
+	a.append(x)
 
-		q = '''select TOP(1) InName from ''' + dbTableName + ' where InSR70 = \'' + stationId + '\''
-		dbStationsWithCount.append( pd.read_sql_query(q, conn)['InName'].values[0] + " [" + str(len(x)) + "]")
+	q = '''select TOP(1) InName from ''' + dbTableName + ' where InSR70 = \'' + stationId + '\''
+	dbStationsWithCount.append( pd.read_sql_query(q, conn)['InName'].values[0] + " [" + str(len(x)) + "]")
 
-	print(dbStationsWithCount)
-	d = pd.DataFrame([*zip(*a)], columns=dbStationsWithCount)
-	d.plot.box(grid='True', showfliers=False)
-	plt.xticks(rotation=35)
-	#plt.title("Meskania v staniciach podla stanice\nDelay <> 0")
-	plt.title("Meskania v staniciach podla stanice")
-	plt.show()
+d = pd.DataFrame(itertools.zip_longest(*a), columns=dbStationsWithCount)
+d.plot.box(grid='True', showfliers=False)
+plt.xticks(rotation=35)
+plt.title(dbTableName)
+plt.xlabel('Station name')
+plt.ylabel('Delay [s]')
+plt.show()
